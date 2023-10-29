b787e555a2b23c1b6913a60b92fea5631f57f183
Abdullah Alawad <abdullahalawad123321@gmail.com>
2023-10-29 11:05:24 +0300

    Tests are Done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,50 @@
 import React, { useState, useEffect } from "react";
 
 function App() {
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState("");
+  const [image, setImage] = useState([]);
+  //const isFives = false;
+
+  useEffect(
+    //() => console.log("useEffect called"),
+    () => {
+      fetch("https://dog.ceo/api/breeds/image/random/3")
+      .then((r) => r.json())
+      .then((data) => setImage(data.message))
+    },
+    [count]
+  )
+
   useEffect(() => {
-    console.log("useEffect called");
-  });
+    document.title = text
+  }, [text]
+  )
+
+  useEffect(() => {
+    setTimeout(() => setCount(0), 5000)
+  }, [])
 
   console.log("Component rendering");
 
-  return <button>Click Me</button>;
+  return <div>
+            <button onClick={() => setCount((count) => count + 1)}>
+              I have clicked this button {count} times
+            </button>
+
+            <input 
+              type="text"
+              placeholder="type..."
+              value={text}
+              onChange={(e) => setText(e.target.value)}
+            />
+
+            <div>
+              {image.map((image) => (
+                <img src={image} key={image} alt={image} />
+              ))}
+            </div>
+          </div>
 }
 
 export default App;
